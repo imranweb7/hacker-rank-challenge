cfc840e2d73f2540d90c0878e9d85d0de898c9af
Muhammed Imran Hussain <imranweb7@gmail.com>
2016-10-10 19:09:05 +0700

    Little Ashishs Huge Donation

--- little_chammys_huge_donation.py
+++ little_chammys_huge_donation.py
@@ -1,50 +1,35 @@
-# Your Python Path Here
+﻿# Your Python Path Here
 # https://www.hackerrank.com/challenges/little-chammys-huge-donation
 # Divide and Conquer
-
 import math;
 
 list_square_summation = [0];
 
 def findNumberOfChildrenServed(n, start, end):
-    if(list_square_summation[start] == n):
-        return start;
-
-    if (list_square_summation[end] == n):
-        return end;
-
-    if((end - start) == 1 and list_square_summation[start] < n and list_square_summation[end] > n):
-        return start;
-
-    if (list_square_summation[start] < n and list_square_summation[end] > n):
-        mid = math.ceil((start + end) / 2);
-
-        left = findNumberOfChildrenServed(n, start, mid);
-
-        if(not left):
-            return findNumberOfChildrenServed(n, mid+1, end);
-        else:
-            return left;
-    else:
-        return 0;
-
+	if(list_square_summation[start] == n): return start;
+	if (list_square_summation[end] == n): return end;
+	
+	if (list_square_summation[start] < n and list_square_summation[end] > n):
+		if(end - start == 1): return start;			
+		mid = math.ceil((start + end) / 2);
+		right = findNumberOfChildrenServed(n, mid, end);
+		if(right): return right;
+		else: return findNumberOfChildrenServed(n, start, mid);
+	else: return 0;
 
 def getNumberOfChildrenServed(n):
-    result = findNumberOfChildrenServed(n, 0, (len(list_square_summation) - 1));
-
-    if(not result):
-        for i in range(len(list_square_summation), n + 1):
-            list_square_summation.append((i * (i+1) * ((2 * i) + 1)) / 6);
-
-            result = findNumberOfChildrenServed(n, (i-1), (len(list_square_summation) - 1));
-            if(result):
-                break;
-
-    return result;
-
+	result = findNumberOfChildrenServed(n, 0, (len(list_square_summation) - 1));
+	if(result): return result;
+	
+	for i in range(len(list_square_summation), n + 1):
+		sum = int(((i * (i+1) * ((2 * i) + 1)) / 6));
+		list_square_summation.append(sum);
+		if(sum == n): return i;
+		if(sum > n): return i - 1;
+	return 0;
 
 T = int(input());
 while(T > 0):
-    X = int(input());
-    print(getNumberOfChildrenServed(X));
-    T -= 1;
+	X = int(input());
+	print(getNumberOfChildrenServed(X));
+	T -= 1;
